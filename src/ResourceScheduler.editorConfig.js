08f698e5d6b3d34b1e7aa9d9879b360c77319760
Hunter Koppen <hunter.koppen@kobeon.nl>
2023-08-18 11:26:13 +0200

    previewmode

--- src/ResourceScheduler.editorConfig.js
+++ src/ResourceScheduler.editorConfig.js
@@ -13,3 +13,65 @@ export function getProperties(values, defaultProperties, target) {
     }
     return defaultProperties;
 }
+
+export function getPreview(values, isDarkMode) {
+    console.log(values);
+    const titleHeader = {
+        type: "RowLayout",
+        columnSize: "fixed",
+        borders: true,
+        borderWidth: 1,
+        children: [
+            {
+                type: "Container",
+                padding: 4,
+                children: [
+                    {
+                        type: "Text",
+                        content: "Resource Scheduler"
+                    }
+                ]
+            }
+        ]
+    };
+    const timeLine = {
+        type: "RowLayout",
+        columnSize: "fixed",
+        children: [
+            {
+                type: "Container",
+                borders: true,
+                children: [
+                    {
+                        type: "DropZone",
+                        property: values.groupContent,
+                        placeholder: "Group content, place widgets here"
+                    }
+                ]
+            },
+            {
+                type: "Container",
+                borders: true,
+                grow: 3,
+                children: [
+                    { type: "DropZone", property: values.itemContent, placeholder: "Item content, place widgets here" }
+                ]
+            }
+        ]
+    };
+    const loader = {
+        type: "Container",
+        borders: true,
+        children: [
+            {
+                type: "DropZone",
+                property: values.loadingContent,
+                placeholder: "Loading timeline, content here will be shown while the timeline is loading."
+            }
+        ]
+    };
+    return {
+        type: "Container",
+        children: [titleHeader, timeLine, loader]
+    };
+}
